a1afd542e2aefd9e3fd2b51d91d17e276ec79709
its-nandlal <nandlaljangir41@gmail.com>
2026-03-15 16:48:27 +0530

    Convert api and web to normal directories in monorepo

---
+++ apps/web/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IeBFzwNDeRaEq1jS1DT0SF7PsZ6M0xCzMurhdPEp3xw=\"\n}"
---
+++ apps/web/.next/dev/static/chunks/apps_web_a0ff3932._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_24cc5834._.js",
+  "static/chunks/737ae_next_dist_compiled_react-dom_6f172c58._.js",
+  "static/chunks/737ae_next_dist_compiled_react-server-dom-turbopack_b0d2e18b._.js",
+  "static/chunks/737ae_next_dist_compiled_next-devtools_index_fae381fb.js",
+  "static/chunks/737ae_next_dist_compiled_1938e8d7._.js",
+  "static/chunks/737ae_next_dist_client_74a7aba2._.js",
+  "static/chunks/737ae_next_dist_5013c2c5._.js",
+  "static/chunks/0e610_@swc_helpers_cjs_4798391a._.js"
+],
+    source: "entry"
+});
